87868e9d809691a284bcec6ccf5c8ada86a407da
raskinminsk <raskin.minsk@gmail.com>
2024-01-14 22:56:13 +0300

    update gitignore

--- install_mongodb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#mongodb install script
-sudo apt update
-sudo apt install mongodb -y
-sudo systemctl start mongodb
-sudo systemctl enable mongodb
--- install_ruby.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#Ruby install script
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
