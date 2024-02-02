b24d7dda189489264d76ee9a7ee45813754278cb
raskinminsk <raskin.minsk@gmail.com>
2024-02-02 20:00:56 +0300

    config files commit v4

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,19 +1,21 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#    }
-#  }
-#  required_version = ">= 0.13"
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
 
   backend "s3" {
-    endpoints  = "storage.yandexcloud.net"
-    bucket     = "raskinmsk-tfstate"
-    region     = "ru-central1"
-    key        = "terraform1.tfstate"
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket                  = "raskinmsk-tfstate"
+    region                  = "ru-central1"
+    key                     = "terraform1.tfstate"
     shared_credentials_file = "storage.key"
 
     skip_region_validation      = true
     skip_credentials_validation = true
   }
-#}
+}
